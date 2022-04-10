d5abe125b9ac213ac2c666572e2abe63ad0564fa
Utkarsh Agrawal <utkarshblp2k@gmail.com>
2022-04-10 10:57:37 +0530

    initial commit

--- admin/process_del_book.php
+++ admin/process_del_book.php
@@ -1,6 +1,7 @@
 <?php
 require('includes/config.php');
 
+
 			$query="delete from book where b_id =".$_GET['b_id'];
 		
 			mysqli_query($conn,$query) or die("can't Execute...");
--- admin/process_del_contact.php
+++ admin/process_del_contact.php
@@ -2,6 +2,7 @@
 
 	require('includes/config.php');
 			
+			$con = mysqli_connect("localhost", "root", "root", "book_store");
 			$query="delete from contact where con_id =".$_GET['id'];
 		
 			mysqli_query($conn,$query) or die("can't Execute...");
